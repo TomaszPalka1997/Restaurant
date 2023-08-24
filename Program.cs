41eee3807a52b96606894dbe76b3f4e213c753b6
Tomasz Palka <tomasz.palka1997@gmail.com>
2023-08-24 17:41:48 +0200

    created new test branch

--- Program.cs
+++ Program.cs
@@ -1,17 +1,37 @@
 ﻿using Restaurant.Meals.Soup;
 using Restaurant.Meals.Pizza;
+using Restaurant;
+using Restaurant.Meals;
 
-Console.WriteLine("Hello in the restaurant");
-
-var dishes = new List<Restaurant.Interfaces.IDish>();
-
-dishes.Add(new TomatoSoup());
-dishes.Add(new Margherita());
-
-foreach (Restaurant.Interfaces.IDish dish in dishes)
+while (true)
 {
-    dish.ShowIngredients();
+    var dishes = new List<Restaurant.Interfaces.IDish>();
+    var settlement = new Settlement();
+
+    UserInterface();
+    TakeOrder(dishes);
+
+    dishes.Add(new TomatoSoup());
+    dishes.Add(new Margherita());
+
+    ShowIngredients(dishes);
+    break;
 }
 
+void UserInterface()
+{
+    Console.WriteLine("Hello in the restaurant");
+}
+void TakeOrder(List<Restaurant.Interfaces.IDish> dish)
+{
+    dish.Add(new TomatoSoup());
+}
+void ShowIngredients(List<Restaurant.Interfaces.IDish> dish)
+{
+    foreach (Restaurant.Interfaces.IDish dishh in dish)
+    {
+        dishh.ShowIngredients();
+    }
+}
 Console.ReadLine();
 
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Project representing menu from known restaurant.
 To do:
+UI
 finish implementing restaurant objects
 colections
 generic type
